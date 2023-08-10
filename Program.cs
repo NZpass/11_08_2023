8872e652a30b9b25ebdade5237a67b8cf3a0f3f5
GJ <GJ@dog.su>
2023-08-10 19:23:39 +0300

    Третье число

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число   ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA >=100 && numberA <1000)
+{
+int X = numberA % 10;
+Console.WriteLine("Третья цифра числа является  "+ X);
+}
+else
+Console.WriteLine("Третьей цифры нет");
